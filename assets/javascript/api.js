e457830fc0478874bb26eedbffdd36b91f3819f2
Sally Beckwith <sallypbeckwith@gmail.com>
2019-03-30 12:55:32 -0400

    initial commit

---
+++ assets/javascript/api.js
@@ -0,0 +1,132 @@
+
+// temporary function attached to button to gather data from API
+$("#spoonacular-search").on("click", function(event) {
+  event.preventDefault();
+  var items = ["beef","cashews","onions"];
+  getRecipes(items, 3);
+})
+
+// ========  MULTIPLE RECIPES API CALL =======================================
+/*
+ *  Makes AJAX call to get the recipes based on the search criteria provided.
+ *  Parameters:
+ *    ingredients:   an array of ingredients to include in the recipe
+ *    quantity:      the number of recipes to return 
+ */
+const getRecipes = (ingredients, quantity) => {
+  // The next line changes the array into a string where each item is separated by %2C and any
+  //   spaces in the search ingredients are replaced by a + 
+  var convertedString = ingredients.join("%2C").replace(/ /g,"+");
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=${quantity}&ranking=1&ignorePantry=false&ingredients=${convertedString}`,
+    "method": "GET",
+    "headers": {
+      "X-RapidAPI-Key": "13639e06b5msh6d9dc61e3c615b9p1f0efcjsn980ee447f232",
+      "cache-control": "no-cache",
+      "Postman-Token": "93e85d06-a15d-45b5-b778-3914b88e4fe0"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    var recipes = createRecipeArray(response);
+    // TODO:  create recipe objects and put them in the recipe display region 
+    console.log("RECIPE CHOICES:");
+    console.log(recipes);
+  });
+}
+
+// Generates an array of recipe objects. Each object contains the recipe ID, title, and image url.
+function createRecipeArray(resp) {
+  var recipes = [];
+  resp.forEach((o) => {
+    recipes.push({
+      "id": o.id,
+      "title": o.title,
+      "image": o.image,
+    })
+  })
+  return recipes;
+}
+
+// ======== SINGLE RECIPE API CALL ======================================
+/*
+ *  Makes AJAX call to get the recipe ingredients and instructions based on the recipe ID number in spoonacular.
+ *  Parameters:
+ *    id:   the integer id value representing a apecific spoonacular recipe
+ */
+const getRecipeDetails = (id) => {
+
+  var recipe = {};
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`,
+    "method": "GET",
+    "headers": {
+      "X-RapidAPI-Key": "13639e06b5msh6d9dc61e3c615b9p1f0efcjsn980ee447f232",
+      "cache-control": "no-cache",
+      "Postman-Token": "93e85d06-a15d-45b5-b778-3914b88e4fe0"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    recipe = createRecipeObject(response);
+    console.log("RECIPE 1 DETAILS:");
+    console.log(recipe);
+  });
+
+}
+
+/* Generates a recipe object with the following key values:
+*  name:  recipe name
+*  image: url of image
+*  timeInMinutes:  total time recipe takes
+*  ingredients:  array contining ingredients used in recipe
+*  instructions: array of steps
+*/
+function createRecipeObject(resp) {
+  let instructions = [];
+  if (resp.analyzedInstructions.length === 0) {
+    instructions = ["No instructions provided"]
+  } else {
+    instructions = getInstructions(resp.analyzedInstructions[0].steps);
+  }
+  const ingredients = getIngredients(resp.extendedIngredients);
+  const recipe = {
+    "name": resp.title,
+    "image": resp.image,
+    "timeinMinutes": resp.readyInMinutes,
+    "ingredients": ingredients,
+    "instructions": instructions,
+  }
+  return recipe;
+}
+
+/*
+* 'steps' is an array of objects. We only want the one with the key named "step"
+* This function will loop through the array and create a new array with only the
+* step information from each object in the array.
+*/
+function getInstructions(steps) {
+  let newArray = [];
+  steps.forEach((s) => {
+    newArray.push(s.step);
+  })
+  return newArray;
+}
+
+/*
+* 'items' is an array of objects. We only want the one with the key named "originalString"
+* This function will loop through the array and create a new array with only the
+* originalString information from each object in the array.
+*/
+function getIngredients(items) {
+  let newArray = [];
+  items.forEach((i) => {
+    newArray.push(i.originalString)
+  })
+  return newArray;
+}
---
+++ assets/javascript/modal.js
@@ -0,0 +1,135 @@
+var ingredients = [];   // this will contain ingredient objects (with name and image)
+
+var defaultIngredients = ['chicken', 'beef', 'cheese', 'salmon'];
+var searchItems = [];
+const defaultImage = "./assets/images/groceries.png"; 
+
+/**
+ * When an item in the selected items list is clicked, remove it from the searchItems array and update the list
+ */
+$(document).on("click", ".selected-item", function() {
+  let food = $(this).attr("food-item");
+  let index = searchItems.indexOf(food);
+  searchItems.splice(index,1);
+  updateSelectedItemsList("#selected-list");        // update modal list
+  updateSelectedItemsList("#main-selected-list");   // update main page list
+})
+
+/**
+ * When an item is clicked, add it to the searchItems array and update the selected items list
+ */
+$(document).on("click", ".single-option", function() {
+
+  let food = $(this).attr("food-item");
+  if (!searchItems.includes(food)) {
+    searchItems.push(food);
+    updateSelectedItemsList("#selected-list");
+  }
+})
+
+  /*
+  * Creates clickable images and puts them in the ingredients-options box
+  */
+ const createIngredientChoices = () => {
+   let $ingredientOptions = $('#ingredient-options');
+   $ingredientOptions.empty();
+   let foodImage = "";
+   for (let i = 0; i < ingredients.length; i++) {
+
+     if (ingredients[i].image !== "") {
+       try {
+         foodImage = `<img id="ingredient-${i}"
+          class="ingredient-option-image"
+          src=${ingredients[i].image}
+          onerror="this.src='${defaultImage}'" >`
+       } catch {
+         foodImage = "";
+       }
+     }
+
+    $('#ingredient-options').append(`<div id="ingredient-${i}" class="m-1 float-left ingredient-frame">
+      <div class="single-option" food-item="${ingredients[i].name}">
+        <div class="text-center ingredient-name">${ingredients[i].name}</div>
+        ${foodImage}
+      </div>`);
+   }
+ }
+
+  const updateSelectedItemsList = (divId) => {
+    const $selectedList = $(divId);
+    $selectedList.empty();  // clear out current list
+    if (searchItems) {
+      let $listGroup = $selectedList.append("<div>").addClass("list-group");
+      for (i=0; i<searchItems.length; i++) {
+        $listGroup.append(`<button type="button" class="list-group-item list-group-item-action w-50 mx-auto text-center py-1 selected-item" food-item="${searchItems[i]}">${searchItems[i]}</button>`);
+      }
+    }
+  }
+
+  // Do the AJAX call to get recipes. Initially set it to find 6 recipes
+  $("#search-api").on("click", function() {
+    // TEMPORARILY COMMENT OUT THE CALL TO GET THE RECIPES
+   // getRecipes(searchItems, 6);
+    $("#select-ingredients").modal('hide');
+    updateSelectedItemsList("#main-selected-list");
+    createMainPageButtons()
+  })
+
+function createMainPageButtons() {
+  let $mainIngredientBtns = $("#fav-btns").append("<div>");
+  // let $mainIngredientBtns = $("#fav-btns").append("<div>").addClass("list-group");
+  for (i=0; i<ingredients.length; i++) {
+    $mainIngredientBtns.append(`<button type="button" class="btn btn-primary text-center py-1 m-2 selected-item" food-item="${ingredients[i].name}">${ingredients[i].name}</button>`);
+  }  
+}
+
+// On click of add ingredient button
+  $("#add-ingredient").on("click", (e) => {
+    e.preventDefault();
+    var subject = $("#new-ingredient-input").val().trim();
+    if (subject !== "" && !ingredientExists(subject)) {
+      ingredients.push(createIngredientObject(subject))
+      createIngredientChoices();
+    }
+    $("#new-ingredient-input").val('');    // clear input field
+  })
+
+  // Returns true if ingredient exists in list, otherwise returns false
+  function ingredientExists(item) {
+
+    var index = ingredients.filter((obj) => {
+      return obj.name === item;
+    })
+    if (index.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function createIngredientObject(item) {
+    return (
+      {
+        "name": item,
+        "image": getIngredientImage(item),
+      }
+    )
+  }
+
+  function getIngredientImage(food) {
+    let findImage;
+    try {
+      findImage = `https://www.themealdb.com/images/ingredients/${food}.png`;
+    } catch {
+      findImage = '';
+    }
+    return findImage
+  }
+
+  $(document).ready(() =>{
+    defaultIngredients.forEach((item) => {
+      ingredients.push(createIngredientObject(item))
+      createIngredientChoices();
+    })    
+
+  })
